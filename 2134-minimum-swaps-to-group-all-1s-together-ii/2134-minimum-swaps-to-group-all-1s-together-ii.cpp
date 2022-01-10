931a7537d9184a5d3d23c0c1f0afa89f9c9c724e
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-01-10 20:23:43 +0530

    Time: 146 ms (12.50%), Space: 80.5 MB (100.00%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -3,7 +3,8 @@ public:
     int minSwaps(vector<int>& nums) {
         int numslen = nums.size(), countofOnes = 0, noofHoles = INT_MAX;
         int totalOnes = count(nums.begin(), nums.end(), 1);
-        
+        // Sliding window is formed and no of holes/zeroes are counted in the window on every iteration.
+        // That is min no of swaps required in that window.
         for (int start = 0, end = 0; start < numslen; start++) {
             while (end - start < totalOnes) {
                 int currentIndex = end % numslen;
