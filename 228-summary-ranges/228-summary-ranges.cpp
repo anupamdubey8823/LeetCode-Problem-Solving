0e221c8c75d8a305e1b37a789e00bfa08114c561
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-02-28 20:55:49 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (62.02%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -5,7 +5,7 @@ public:
         string range = "";
         vector<string> summary;
 
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < len; ) {
             start = end = i;
             while (end < len-1 && nums[end] + 1 == nums[end+1])
                 end++;
@@ -15,7 +15,7 @@ public:
                 range = to_string(nums[start]) + "->" + to_string(nums[end]);
             summary.emplace_back(range);
             range = "";
-            i = end;
+            i = end + 1;
         }
         return summary;
     }
