da99d9c3d693509d73c5d0a101f0ae8cf9f529ba
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-03-01 11:27:57 +0530

    Time: 6 ms (65.69%), Space: 7.9 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -6,7 +6,7 @@ public:
         for (int i = 1; i <= n; i++) {
             // if (i & 1) ans[i] = ans[i/2] + 1;
             // else ans[i] = ans[i/2];
-            ans[i] = ans[i/2] + (i & 1);
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
     }
