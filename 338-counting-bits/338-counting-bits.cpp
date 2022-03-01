5d8ce5ff973b11a48a8fadb072f972b2ec89c1fb
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-03-01 10:50:37 +0530

    Time: 7 ms (62.32%), Space: 7.7 MB (98.10%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,8 +4,9 @@ public:
         vector<int> ans(n+1);
         ans[0] = 0;
         for (int i = 1; i <= n; i++) {
-            if (i & 1) ans[i] = ans[i/2] + 1;
-            else ans[i] = ans[i/2];
+            // if (i & 1) ans[i] = ans[i/2] + 1;
+            // else ans[i] = ans[i/2];
+            ans[i] = ans[i/2] + (i & 1);
         }
         return ans;
     }
