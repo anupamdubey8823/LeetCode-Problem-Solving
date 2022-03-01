4e189a6ded905f15112ac459be2d3b810c843f19
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-03-01 10:47:44 +0530

    Time: 4 ms (88.16%), Space: 7.8 MB (89.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,20 +1,11 @@
 class Solution {
-    int findOnesCount(int num) {
-        int ones = 0;
-        while (num != 0 && num != 1) {
-            if (num % 2 == 1)
-                ones++;
-            num /= 2;
-        }
-        if (num == 1) return ones + 1;
-        return ones;
-    }
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
-        for (int i = 0; i <= n; i++) {
-            int ones = findOnesCount(i);
-            ans.emplace_back(ones);
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i & 1) ans[i] = ans[i/2] + 1;
+            else ans[i] = ans[i/2];
         }
         return ans;
     }
