8eca62e9958c70dc078e5bf8b94833e69c6d8a84
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-03-21 23:49:09 +0530

    Time: 4 ms (72.57%), Space: 6.9 MB (13.71%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -5,23 +5,19 @@ public:
         map<char, int> lastIndex;
         for (int i = 0; i < len; i++) {
             lastIndex[s[i]] = i;
-            cout << lastIndex[s[i]] << " ";
-            
         }
         vector<int> partitions;
-        for (int i = 0; i < len; i++) {
-            // while (i <= currPartition) {
-            //     currPartition = max(currPartition, lastIndex[s[i]]);
-            //     i++;
-            // }
-            currPartition = max(currPartition, lastIndex[s[i]]);
-            if (currPartition == i) {
-                partitions.emplace_back(currPartition - prevPartition);
-                prevPartition = currPartition;
+        for (int i = 0; i < len;) {
+            while (i < len && i <= currPartition) {
+                currPartition = max(currPartition, lastIndex[s[i]]);
+                i++;
             }
+            partitions.emplace_back(currPartition - prevPartition);
+            prevPartition = currPartition;
             if (currPartition == len) {
                 break;
             }
+            currPartition = i;
         }
         
         return partitions;
