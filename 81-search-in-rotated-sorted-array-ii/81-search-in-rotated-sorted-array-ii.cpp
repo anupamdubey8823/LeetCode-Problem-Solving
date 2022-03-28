7c00379badad9cd814a9bfee03617fb53aa8b205
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-03-28 16:16:07 +0530

    Time: 17 ms (10.40%), Space: 13.8 MB (90.12%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,26 @@
 class Solution {
+    bool binSearch(int low, int high, vector<int> &nums, int target) {
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+            if (nums[mid] == target) {
+                cout << nums[mid] << " ";
+                return true;
+            }
+            else if (target > nums[mid])
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return false;
+    }
 public:
     bool search(vector<int>& nums, int target) {
-        for (auto &num: nums)
-            if (target == num)
-                return true;
+        int i = 0, numLen = nums.size();
+        while (i+1 < numLen && nums[i] <= nums[i+1])
+            i++;
+//         Now we got [0,i] and [i+1, numLen) sorted search spaces. Hence, we apply Binary Search on both of 'em.
+        if (binSearch(0, i, nums, target) || binSearch(i+1, numLen-1, nums, target))
+            return true;
         return false;
     }
 };
