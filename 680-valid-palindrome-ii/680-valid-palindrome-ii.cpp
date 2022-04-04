7f1d6430d2c4defe770322266c4161d32509ee3c
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-04-04 15:26:41 +0530

    Time: 48 ms (98.38%), Space: 24.1 MB (17.24%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,13 +1,20 @@
 class Solution {
+    bool checkPalindrome(string s, int start, int end) {
+        while (start < end) {
+            if (s[start] != s[end])
+                return false;
+            start++, end--;
+        }
+        return true;
+    }
 public:
     bool validPalindrome(string s) {
-        for (int i = 0, j = s.size() - 1; i < j; i++, j--)
-            if (s[i] != s[j]) {
-                int i1 = i, j1 = j - 1, i2 = i + 1, j2 = j;
-                while (i1 < j1 && s[i1] == s[j1]) {i1++; j1--;};
-                while (i2 < j2 && s[i2] == s[j2]) {i2++; j2--;};
-                return i1 >= j1 || i2 >= j2;
-            }
+        int start = 0, end = s.size() - 1;
+        while (start < end) {
+            if (s[start] != s[end]) 
+                return checkPalindrome(s, start+1, end) || checkPalindrome(s, start, end-1);
+            start++, end--;
+        }
         return true;
     }
 };
