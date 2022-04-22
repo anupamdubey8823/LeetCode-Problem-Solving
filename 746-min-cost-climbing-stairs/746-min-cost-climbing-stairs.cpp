8d5b0114290dfe9d51b26ae28610b9f2387d8589
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-04-22 13:25:36 +0530

    Time: 11 ms (42.99%), Space: 13.9 MB (12.99%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -10,6 +10,7 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         int len = cost.size();
         vector<int> minCost(len, -1);
+//         Here, we didn't make dp array of size len+1 because max index which we start from is len-1 whose length = len. Hence dp array size = len instead of len+1. 
         return min(minCostUtil(cost, len-1, minCost), minCostUtil(cost, len-2, minCost));
     }
 };
