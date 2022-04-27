bc1421af554cf0bf2cac2ab9864c8464191978f9
Anupam Dubey <59842200+anupamdubey8823@users.noreply.github.com>
2022-04-27 21:44:20 +0530

    Time: 25 ms (60.02%), Space: 16.5 MB (81.37%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int farthest = 0, left = 0, right = 0, minJumps = 0, len = nums.size();
+        // Greedy Approach - O(N) time and O(1) space.
+        int maxReachable = 0, left = 0, right = 0, minJumps = 0, len = nums.size();
         while (right < len - 1) {
             for (int i = left; i <= right; i++) {
-                farthest = max(farthest, i + nums[i]);
+                maxReachable = max(maxReachable, i + nums[i]);
             }
             left = right+1;
-            right = farthest;
-            minJumps++;
+            right = maxReachable;
+            minJumps++; // Each time we are updating our range i.e. jump from one range to the next. Hence increasing minJumps;
         }
         return minJumps;
     }
